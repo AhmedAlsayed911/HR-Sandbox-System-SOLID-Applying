f8b623ab4f57ace67c614cf5e164bd35ae5c1e8d
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 22:15:25 +0200

    Add generic repository interfaces

---
+++ OOP_HRSystem/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem
+{
+    internal interface IRepository<T> where T : Entity
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+    }
+}
---
+++ OOP_HRSystem/IUpdatableRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem
+{
+    internal interface IUpdatableRepository<T> where T : Entity
+    {
+        void Update(T entity);
+    }
+}
