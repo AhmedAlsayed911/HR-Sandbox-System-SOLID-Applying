56cad6f545fbc618cbd101c565ece0448e8c07b9
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 22:14:09 +0200

    Implement employee hierarchy (applicant,employee)

---
+++ OOP_HRSystem/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem
+{
+    internal abstract class Employee : Person
+    {
+        public string Email { get; internal set; }
+
+        // private Decimal _basicSalary;
+        // public Decimal BasicSalary
+        // {
+        //     get
+        //     {
+        //         return _basicSalary;
+        //     }
+        //     set
+        //     {
+        //         if (value < 2500)
+        //             throw new ArgumentException("Invalid Salary!");
+        //         _basicSalary = value;
+        //     }
+        // }
+        // public int TaxPercentage {  get; set; }      
+        //public abstract decimal Calculate(Employee employee);
+        public abstract decimal GetSalary();
+        public abstract IEnumerable<PayItem> GetPayItems();     
+        
+    }
+}
