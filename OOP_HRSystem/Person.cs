e8c5b29fd963ab7c46384d038b53e138038321a0
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 22:13:07 +0200

    Add base entity and person models

---
+++ OOP_HRSystem/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem
+{
+    internal abstract class Person : Entity
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public void setName(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentNullException("Invalid Name!");
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        private DateOnly _birthDate;
+        public DateOnly BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                if (value < new DateOnly(1986, 9, 14))
+                    throw new ArgumentException("Invalid Birth Date!");
+                _birthDate = value;
+            }
+        }
+    }
+}
