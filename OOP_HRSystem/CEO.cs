83133cacb2c1b1f4299cd5a6291cac2b6d428586
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 22:15:17 +0200

    Implement employee hierarchy (salaried,CEO,DismissableEmp)

---
+++ OOP_HRSystem/CEO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem
+{
+    internal class CEO : Employee
+    {
+        public override IEnumerable<PayItem> GetPayItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOP_HRSystem/SalariedEmployee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem
+{
+    internal class SalariedEmployee : DissmisableEmployee
+    {
+        public decimal BasicSalary { get; set; }
+        public decimal Transportation { get; set; }
+        public decimal Housing { get; set; }
+        public override decimal GetSalary()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return BasicSalary + Transportation + Housing;   
+        }
+        public decimal GetSalary(int taxPercentage)
+        {
+            return GetSalary() - (BasicSalary * taxPercentage / 100);
+        }
+        public decimal GetSalary(int taxPercentage , int bonus)
+        {
+            return GetSalary(taxPercentage) + bonus;
+        }
+        public override IEnumerable<PayItem> GetPayItems()
+        {
+            return new[]
+            {
+                new PayItem("Total hours : ", BasicSalary),
+                new PayItem("Transportaion : ",Transportation),
+                new PayItem("Housing : ",Housing)
+            };
+        }
+    }
+}
