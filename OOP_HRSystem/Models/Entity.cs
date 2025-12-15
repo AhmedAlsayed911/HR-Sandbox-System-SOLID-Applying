3256901197239894c4f965e557d0b93e1964c7cf
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-15 05:49:56 +0200

    refactor: Move core domain models to Models folder
    - Move Entity.cs, Person.cs, Employee.cs to Models/
    - Move Applicant.cs and PayItem.cs to Models/
    - Update namespace to OOP_HRSystem.Models

--- OOP_HRSystem/Models/Entity.cs
+++ OOP_HRSystem/Models/Entity.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_HRSystem
+namespace OOP_HRSystem.Models
 {
-    internal class Applicant : Person
+    internal abstract class Entity
     {
-                
+        public int Id {  get; set; }
     }
 }
