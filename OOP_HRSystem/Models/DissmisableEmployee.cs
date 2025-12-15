d473cdf0e56d13ca16adf5bad7e443c2f00e7db6
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-15 05:50:06 +0200

    refactor: Move employee types to Models folder
    - Move SalariedEmployee, HourlyEmployee, InternEmployee to Models/
    - Move CEO and DissmisableEmployee to Models/
    - Organize all employee hierarchies in one place
    - Update namespace to OOP_HRSystem.Models

---
+++ OOP_HRSystem/Models/DissmisableEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_HRSystem.Models
+{
+    internal class DissmisableEmployee : Employee
+    {
+       
+        public override IEnumerable<PayItem> GetPayItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+        public bool Dismiss { get; private set; }
+        public void setDismissed()
+        {           
+            Dismiss = true;
+        }
+    }
+}
--- OOP_HRSystem/Models/SalariedEmployee.cs
+++ OOP_HRSystem/Models/SalariedEmployee.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_HRSystem
+namespace OOP_HRSystem.Models
 {
     internal class SalariedEmployee : DissmisableEmployee
     {
